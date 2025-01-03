0279facc9d987462dba7b3d9a9c70cb279dc9a54
Patrick Brown <patrick.barry.brown@gmail.com>
2025-01-03 12:51:24 -0700

    Update Configuration.h
    fix probing margin

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1200,7 +1200,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 5
+#define PROBING_MARGIN 8
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (133*60)
